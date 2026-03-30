38b01e4b625c3fae90646a38c9ef2d443e765a01
AbidAlAzad2001 <shakibprof2001@gmail.com>
2026-03-30 20:07:47 +0600

    first

--- lms_ai/settings.py
+++ lms_ai/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me-in-producti
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
